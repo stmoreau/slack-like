fbc331ac59201c222cff104621a06cdd4ed2b95e
Stephane Moreau <st.moreau@outlook.com.gr>
2017-05-05 02:18:21 +0100

    communicating with websockets

--- src/App.js
+++ src/App.js
@@ -10,14 +10,44 @@ class App extends Component{
       channels: [],
       users: [],
       messages: [],
-      activeChannel: {}
+      activeChannel: {},
+      connected: false
     };
+  }
+  componentDidMount(){
+    let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+    ws.onmessage = this.message.bind(this);
+    ws.onopen = this.open.bind(this);
+    ws.onclose = this.close.bind(this);
+  }
+  message(e){
+    const event = JSON.parse(e.data);
+    if(event.name === 'channel add'){
+      this.newChannel(event.data);
+    }
+  }
+  open(){
+    this.setState({connected: true});
+  }
+  close(){
+    this.setState({connected: false});
+  }
+  newChannel(channel){
+    let {channels} = this.state;
+    channels.push(channel);
+    this.setState({channels});
   }
   addChannel(name){
     let {channels} = this.state;
-    channels.push({id: channels.length, name});
-    this.setState({channels});
     // TODO: Send to server
+    let msg = {
+      name: 'channel add',
+      data: {
+        id: channels.length,
+        name
+      }
+    };
+    this.ws.send(JSON.stringify(msg));
   }
   setChannel(activeChannel){
     this.setState({activeChannel});
