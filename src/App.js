59401a4573cfd048509071badbd3b67c0f400c64
Stephane Moreau <st.moreau@outlook.com.gr>
2017-05-05 20:36:12 +0100

    create socket.js

--- src/App.js
+++ src/App.js
@@ -16,21 +16,7 @@ class App extends Component{
   }
   componentDidMount(){
     let ws = this.ws = new WebSocket('ws://echo.websocket.org');
-    ws.onmessage = this.message.bind(this);
-    ws.onopen = this.open.bind(this);
-    ws.onclose = this.close.bind(this);
-  }
-  message(e){
-    const event = JSON.parse(e.data);
-    if(event.name === 'channel add'){
-      this.newChannel(event.data);
-    }
-  }
-  open(){
-    this.setState({connected: true});
-  }
-  close(){
-    this.setState({connected: false});
+
   }
   newChannel(channel){
     let {channels} = this.state;
